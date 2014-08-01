839bc8cd1acdcc32222a4d20525eba74ce9baf25
Vincent Demeester <vincent+git@demeester.fr>
2014-08-01 23:30:59 +0200

    Use screenlock instead of slock for lock screen

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -598,7 +598,7 @@ root.buttons(awful.util.table.join(
                 awful.button({ }, 3, function () mymainmenu:toggle() end),
                 awful.button({ }, 4, awful.tag.viewnext),
                 awful.button({ }, 5, awful.tag.viewprev),
-                awful.button({ }, 10, function() awful.util.spawn("slock") end)
+                awful.button({ }, 10, function() awful.util.spawn("screenlock") end)
 ))
 -- }}}
 
